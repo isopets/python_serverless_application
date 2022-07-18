53b180b68be7fe06e77104118db3eeb25b62e5af
yuto isogai <jandp.0717@gmail.com>
2022-07-19 04:07:51 +0900

    utils config server create

---
+++ .history/flask_blog/config_20220719035437.py
@@ -0,0 +1,24 @@
+
+import os
+
+class DevelopmentConfig(object):
+    DEBUG = True
+    SECRET_KEY = 'secret key'
+    USERNAME = 'john'
+    PASSWORD = 'due123'
+
+    AWS_ACEESS_KEY_ID = 'AWS_ACEESS_KEY_ID'
+    AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
+    DYNAMODB_REGION = 'ap-northeast-1'
+    DYNAMODB_ENDPOINT_URL = 'http://localhost:8000'
+
+class ProductionConfig(object):
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SERVERLESS_SECRET_KEY')
+    USERNAME = 'john'
+    PASSWORD = os.environ.get('SERVERLESS_USER_PW')
+
+    AWS_ACEESS_KEY_ID = os.environ.get('SERVERLESS_AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('SERVERLESS_AWS_SECRET_KEY')
+    DYNAMODB_REGION = 'ap-northeast-1'
+    DYNAMODB_ENDPOINT_URL = None
---
+++ .history/flask_blog/models/users_20220719034825.py
@@ -0,0 +1,7 @@
+# flask_blog/models/users.py
+
+from flask_login import UserMixin
+
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
